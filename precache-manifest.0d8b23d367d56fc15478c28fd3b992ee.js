4d6b63f8442df2ce9782eb9af34b325232d03f6d
Akexorcist <akexorcist@gmail.com>
2019-11-07 19:14:31 +0700

    Updates

---
+++ precache-manifest.0d8b23d367d56fc15478c28fd3b992ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f85e08275b260303969d3c9304656b",
+    "url": "/nextzy-url-scheme/index.html"
+  },
+  {
+    "revision": "06f446bd395a540a37d8",
+    "url": "/nextzy-url-scheme/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "c85c44b1782c005fb8e7",
+    "url": "/nextzy-url-scheme/static/css/main.38be337b.chunk.css"
+  },
+  {
+    "revision": "06f446bd395a540a37d8",
+    "url": "/nextzy-url-scheme/static/js/2.b84db2d8.chunk.js"
+  },
+  {
+    "revision": "c85c44b1782c005fb8e7",
+    "url": "/nextzy-url-scheme/static/js/main.fdd25384.chunk.js"
+  },
+  {
+    "revision": "8080a6cf3940fe37fe33",
+    "url": "/nextzy-url-scheme/static/js/runtime-main.0472ffdd.js"
+  }
+]);
