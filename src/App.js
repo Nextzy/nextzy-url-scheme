1efcb0fe7f2305db49e4f014d10decbd71558399
Akexorcist <akexorcist@gmail.com>
2019-11-07 18:59:42 +0700

    Add the url scheme and firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Button, Typography, Layout } from 'antd';
+
+const { Title } = Typography;
+const { Content } = Layout;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title className="Title">my AIS</Title> 
+        <div>
+          <Button className="Button" size="large" type="primary" href="myais://CurrentPackage">Current Package</Button>
+        </div>
+        <Title className="Title">Easy App</Title> 
+        <div>
+          <Button className="Button" size="large" type="primary" href="neweasyapp://Home">Home</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://PrepaidIdent">PI</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://RePrepaidIdent">Re PI</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://TopUp">Top Up</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://TopUpRefund">Top Up Refund</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://VasPackage">VAS Package</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://ConvertPreToPost">Convert Pre to Post</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://NewRegister">New Register</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://MNP">MNP</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://PoolingNumber">PoolingNumber</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://SallerNews">SallerNews</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://Setting">Setting</Button><br />
+          <Button className="Button" size="large" type="primary" href="neweasyapp://News?id=5d550bfce316f76eec9d02cf">News</Button>
+        </div>
       </header>
     </div>
   );
